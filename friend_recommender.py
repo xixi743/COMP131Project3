235b45ea541a97c7a523c57efe191911cd24a3ff
Unknown <halea@oxy.edu>
2017-12-09 17:16:51 -0800

    What we did in the Lib

--- friend_recommender.py
+++ friend_recommender.py
@@ -14,6 +14,11 @@ class SocialNetwork:
         Returns:
             [str]: A list of usernames
         '''
+        #users = []
+        #for friend in self:
+         #   if friend not in users:
+          #      users.append(friend)
+        #return users.join(str(x) for x in users)
         return self.users.keys()
 
     def add_user(self, user):
@@ -28,6 +33,7 @@ class SocialNetwork:
             None
         '''
         self.users[user] = []
+
 
     def add_friend(self, user, friend):
         '''Adds a friend to a user
@@ -47,6 +53,7 @@ class SocialNetwork:
         '''
         self.users[user].append(friend)
 
+
     def get_friends(self, user):
         '''Get the friends of a user
 
@@ -60,42 +67,73 @@ class SocialNetwork:
         friends = self.users[user]
         return friends
 
-    def jaccard_index(self, user):
-        #for friend in self.users[user]:
-        #    i = 0
-        #    j = 0
-        #    if i < len(self.users[user]):
-        #        c = 0
-        #        if self.users[user[i]] == self.users[friend[j]]:
-        #            c += 1
-        #        j += 1
-        #    i += 1
-        #    jac = c / len(self.users[user])
-        #return jac
-        for friend in self.users[user]:
-            i = 0
+    def jaccard_index(self, user_1, user_2):
+        # find the amount of users in common
+        c = 0
+        i = 0
+        if i < len(self.users.get(user_1)):
             j = 0
-            v = 0
-            AllFriends = []
-            if i <len(self.users[user]):
-                if friend not in AllFriends:
-                    AllFriends.append(friend)
+            if j < len(self.users.get(user_2)):
+                if self.users.get(user_1)[i] == self.users.get(user_2)[j]:
+                    c += 1
+                j += 1
+            i += 1
+        # find the amount of friends
+        friends = len(self.users.get(user_1)) + len(self.users.get(user_2)) - c
+        # divide common by friends
+        result = c / friends
+
+        return result
+
+    def followers(self, user):
+        # initialize number
+        n = 0
+        # write for loop that sees if user is following and adds one to number
+        # alexis hates for loops
+        # hil thinks for loop is the best choice for this function
+        # alexis is ok with that but hil should probably write this part
+        # hil agrees
+        # robby: stop talking in the third person everybody
+        # alexis says shut up
+        # hil is wondering why her wifi is SO BAD
+        for k in self.users:
+            for v in self.users[k]:
+                if user in v:
+                    n += 1
+        # return number of followers
+        return n
 
     def suggest_friend(self, user):
-        '''Suggest a friend to the user
-        See project specifications for details on this algorithm.
-        Arguments:
-            user (str): The username of the user to find a friend for
+        # initialize
+        p = " "
+        s = user
+        max = 0
+        # create for loop that tests the jaccard index of each other user and finds the highest one
+        # so many for loops why
+        # shut up
+        for person in self.list_users():
+            if person != s:
+                x = self.jaccard_index(s, person)
+                if x > max:
+                    max = x
+                    p = person
+        max2 = 0
+        best = ""
+        # create for loop that finds the best choice to recommend
+        # noooo
+        for friend in self.users[p]:
+            if friend not in self.users[user]:
+                if self.followers(friend) > max2:
+                    max2 = self.followers(friend)
+                    best = friend
+                return best
+            #else:
+             #   return None
 
-        Returns:
-            str: The username of a new candidate friend for the user
-        '''
-        pass # FIXME
-        JaccardIndexes = {}
-        for jac in self.users:
 
 
 
+    # don't touch this one Alexis
     def to_dot(self):
         result = []
         result.append('digraph {')
@@ -129,9 +167,9 @@ def create_network_from_file(filename):
 
 
 def main():
-    network = create_network_from_file('simple.network')
+    network = create_network_from_file('intermediate.network')
     print(network.to_dot())
-    print(network.suggest_friend('francis'))
+    print(network.suggest_friend('tory'))
 
 
 if __name__ == '__main__':
